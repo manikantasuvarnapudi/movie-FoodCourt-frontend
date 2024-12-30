99fc2f9064212aa8d76667602932c9725c16d03f
manikanta <smanikanta515@gmail.com>
2024-12-30 08:32:26 +0530

    increase and descream count added1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, createContext, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import Home from "./components/Home";
 import FoodItemDetails from "./components/FoodItemDetails";
 import Cart from "./components/Cart";
