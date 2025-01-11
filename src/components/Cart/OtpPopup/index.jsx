8345a2d59215b5d900941d19d261b15d53175a55
manikanta <smanikanta515@gmail.com>
2025-01-11 12:35:57 +0530

    autoread added

--- src/components/Cart/OtpPopup/index.jsx
+++ src/components/Cart/OtpPopup/index.jsx
@@ -1,15 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import './index.css';
-
 
 const OtpPopup = ({ isOpen, onClose, onVerify }) => {
   const [otp, setOtp] = useState('');
 
+  useEffect(() => {
+    if ("OTPCredential" in window) {
+      navigator.credentials
+        .get({ otp: { transport: ["sms"] } })
+        .then((otpCredential) => {
+          const extractedOtp = otpCredential.code;
+          setOtp(extractedOtp); 
+          onVerify(extractedOtp);
+          console.log("Received OTP:", extractedOtp);
+        })
+        .catch((err) => {
+          console.error("Error reading OTP:", err);
+        });
+    }
+  }, [onVerify]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (otp.trim().length === 6) {
-      onVerify(otp);
+      onVerify(otp); 
     } else {
       alert('Please enter a valid 6-digit OTP');
     }
@@ -23,7 +38,7 @@ const OtpPopup = ({ isOpen, onClose, onVerify }) => {
     >
       <div className="otp-container">
         <h2>OTP Verification</h2>
-        <p>Please enter the OTP sent to your registered <strong>Email</strong> or <strong>phone</strong>.</p>
+        <p>Please enter the OTP sent to your registered <strong>Email</strong> or <strong>Phone</strong>.</p>
         <form onSubmit={handleSubmit}>
           <input
             type="text"
@@ -33,6 +48,7 @@ const OtpPopup = ({ isOpen, onClose, onVerify }) => {
             onChange={(e) => setOtp(e.target.value)}
             className="otp-input"
             inputMode="numeric"
+            autoComplete="one-time-code" 
           />
           <button type="submit" className="otp-button">
             Verify
